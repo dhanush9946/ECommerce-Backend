74c0de72c5b4aab05cd15602a5f6f9c36f260b37
dhanush9946 <dhanushpoothanganam5@gmail.com>
2026-01-07 19:41:40 +0530

    Add project files.

---
+++ ECommerce.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ECommerce.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? PhoneNumber { get; set; }
+        public string PasswordHash { get; set; } = null!;
+        public string Role { get; set; } = "User";
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
